668e48af025d8bacc5149ef95470112e26d566b8
Nikita K <rabot422@gmail.com>
2023-12-14 00:27:19 +0300

    add onChange for all input , to get their data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,57 +1,103 @@
+import { useState } from 'react';
 
 function Form() {
+  const [formData, setformData] = useState({});
+  function getFormData(targetInput) {
+    setformData({
+      ...formData,
+      [targetInput.id]: targetInput.value,
+    });
+    console.log(formData);
+  }
+  
   return (
     <form>
-
       <section className="form_general_info">
         <div>
-            <label htmlFor="inputName">First name: </label>
-            <input value="" type="text" id="inputName" />
+          <label htmlFor="inputName">First name: </label>
+          <input
+            onChange={(event) => {
+              getFormData(event.target);
+            }}
+            value={formData.inputName}
+            type="text"
+            id="inputName"
+          />
         </div>
         <div>
-            <label htmlFor="inputEmail">Email: </label>
-            <input value="" type="email" id="inputEmail" />
+          <label htmlFor="inputEmail">Email: </label>
+          <input
+            onChange={(event) => {
+              getFormData(event.target);
+            }}
+            value={formData.inputEmail}
+            type="email"
+            id="inputEmail"
+          />
         </div>
         <div>
-            <label htmlFor="inputPhoneNumber">Phone number: </label>
-            <input value="" type="tel" id="inputPhoneNumber" />
+          <label htmlFor="inputPhoneNumber">Phone number: </label>
+          <input
+           onChange={(event) => {
+            getFormData(event.target);
+          }}
+          value={formData.inputPhoneNumber}
+          type="tel" id="inputPhoneNumber" />
         </div>
-    </section>
+      </section>
 
       <section className="form_education_info">
         <div>
-            <label htmlFor="inputSchool">School name: </label>
-            <input value="" type="text" id="inputSchool" />
+          <label htmlFor="inputSchool">School name: </label>
+          <input onChange={(event) => {
+              getFormData(event.target);
+            }}
+            value={formData.inputSchool} type="text" id="inputSchool" />
         </div>
         <div>
-            <label htmlFor="inputTitleOfStudy">Title of study: </label>
-            <input value="" type="text" id="inputTitleOfStudy" />
+          <label htmlFor="inputTitleOfStudy">Title of study: </label>
+          <input  onChange={(event) => {
+              getFormData(event.target);
+            }}
+            value={formData.inputTitleOfStudy}  type="text" id="inputTitleOfStudy" />
         </div>
         <div>
-            <label htmlFor="inputDateOfStudy">Date of study: </label>
-            <input value="" type="text" id="inputDateOfStudy" />
+          <label htmlFor="inputDateOfStudy">Date of study: </label>
+          <input onChange={(event) => {
+              getFormData(event.target);
+            }}
+            value={formData.inputDateOfStudy} type="text" id="inputDateOfStudy" />
         </div>
-    </section>
+      </section>
 
       <section className="form_practice_info">
         <div>
-            <label htmlFor="inputCompanyName">Company name: </label>
-            <input value="" type="text" id="inputCompanyName" />
+          <label htmlFor="inputCompanyName">Company name: </label>
+          <input onChange={(event) => {
+              getFormData(event.target);
+            }}
+            value={formData.inputCompanyName} type="text" id="inputCompanyName" />
         </div>
         <div>
-            <label htmlFor="inputPositionTitle">Position title: </label>
-            <input value="" type="text" id="inputPositionTitle" />
+          <label htmlFor="inputPositionTitle">Position title: </label>
+          <input onChange={(event) => {
+              getFormData(event.target);
+            }}
+            value={formData.inputPositionTitle} type="text" id="inputPositionTitle" />
         </div>
         <div>
-            <label htmlFor="inputMainResponsibility">Main responsibility: </label>
-            <input value="" type="text" id="inputMainResponsibility" />
+          <label htmlFor="inputMainResponsibility">Main responsibility: </label>
+          <input onChange={(event) => {
+              getFormData(event.target);
+            }}
+            value={formData.inputMainResponsibility} type="text" id="inputMainResponsibility" />
         </div>
-    </section>
+      </section>
 
-    <button className="reset_form">Clear</button>
-    <button className="save_form">Save</button>
+      <button className="reset_form">Clear</button>
+      <button className="save_form">Save</button>
     </form>
-  )
+  );
 }
 
-export default Form
+export default Form;
